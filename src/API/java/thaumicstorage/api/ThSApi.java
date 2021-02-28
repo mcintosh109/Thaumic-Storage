b795dc8f20e9dfde22528e2f423ebfdb6047a34b
James McIntosh <McIntosh.Networks@outlook.com>
2021-02-28 00:41:32 -0600

    Basic API setup for connection to RS and Thaumcraft

---
+++ src/API/java/thaumicstorage/api/ThSApi.java
@@ -0,0 +1,46 @@
+package thaumicenergistics.api;
+
+/**
+ * The Thaumic Energistics API
+ *
+ * @author BrockWS
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public interface IThEApi {
+
+    /**
+     * Contains all items in Thaumic Energistics
+     *
+     * @return Class which contains item definitions
+     */
+    IThEItems items();
+
+    /**
+     * Contains all blocks/tiles in Thaumic Energistics
+     *
+     * @return Class which contains block/tile definitions
+     */
+    IThEBlocks blocks();
+
+    /**
+     * Contains all upgrades in Thaumic Energistics
+     *
+     * @return Class which contains upgrade definitions
+     */
+    IThEUpgrades upgrades();
+
+    /**
+     * Contains all config options in Thaumic Energistics
+     *
+     * @return Class which contains config options
+     */
+    IThEConfig config();
+
+    /**
+     * Contains all language keys in Thaumic Energistics
+     *
+     * @return Class which contains language keys
+     */
+    IThELang lang();
+}
---
+++ src/API/java/thaumicstorage/api/ThSConfig.java
@@ -0,0 +1,37 @@
+package thaumicenergistics.api;
+
+import java.util.Map;
+
+import appeng.api.config.TerminalStyle;
+import appeng.api.util.IConfigManager;
+
+import thaumicenergistics.api.config.PrefixSetting;
+
+/**
+ * @author BrockWS
+ */
+public interface IThEConfig {
+
+    Map<String, Integer> essentiaContainerCapacity();
+
+    int tickTimeEssentiaImportBusMin();
+
+    int tickTimeEssentiaImportBusMax();
+
+    int tickTimeEssentiaExportBusMin();
+
+    int tickTimeEssentiaExportBusMax();
+
+    int tickTimeEssentiaStorageBusMin();
+
+    int tickTimeEssentiaStorageBusMax();
+
+    TerminalStyle terminalStyle();
+
+    String modSearchPrefix();
+
+    PrefixSetting modSearchSetting();
+
+    String aspectSearchPrefix();
+
+    PrefixSetting aspectSearchSetting();
